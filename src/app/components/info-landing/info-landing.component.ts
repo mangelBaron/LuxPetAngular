a03918bd8bba147d627213d4121e94e94c39ba3d
mangelBaron <miguelbaronm@hotmail.com>
2024-09-18 14:24:32 -0500

    Landing de LuxPet

---
+++ src/app/components/info-landing/info-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-landing.component.html',
+  styleUrl: './info-landing.component.css'
+})
+export class InfoLandingComponent {
+
+}
