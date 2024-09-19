559949a34190d712784cd858fcf18bc67b4c1003
Arkan56 <seb4s.s44@gmail.com>
2024-09-19 07:32:05 -0500

    Clases de java creadas como interfaces en ts

---
+++ src/app/model/droga.ts
@@ -0,0 +1,10 @@
+import { Tratamiento } from "./tratamiento";
+export interface Droga {
+    id?: number;
+    nombre: string;
+    precioCompra: number;
+    precioVenta: number;
+    unidadesDisponibles: number;
+    unidadesVendidas: number;
+    tratamientos: Tratamiento[];
+}
