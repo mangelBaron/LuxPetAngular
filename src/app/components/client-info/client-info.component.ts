32f0b85cda0fdd130df73c6ad29afda6ffc38a2c
mangelBaron <miguelbaronm@hotmail.com>
2024-10-07 20:38:23 -0500

    Trabajando en REST

--- src/app/components/client-info/client-info.component.ts
+++ src/app/components/client-info/client-info.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cliente } from '../../model/cliente';
+import { ClientService } from '../../services/client.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PetService } from '../../services/pet.service';
 
 @Component({
   selector: 'app-client-info',
   standalone: true,
   imports: [],
   templateUrl: './client-info.component.html',
-  styleUrl: './client-info.component.css'
+  styleUrl: './client-info.component.css' 
 })
 export class ClientInfoComponent {
+
+@Input()
+client! : Cliente;
+
+constructor(
+private clientService: ClientService,
+private router: Router,
+private route: ActivatedRoute,
+private mascotaService: PetService
+){}
+
+
+ngOnit(){
+
+  this.route.params.subscribe(params => {
+    const id = Number(params.get('id'));
+  
+    this.clientService.findById(id).pipe(
+      mergeMap(
+        (clientInfo) => {
+          this.client = clientInfo;
+          return this.mascotaService.findClientPet(this.client.id);
+        }
+      )
+    )
+
+}
+
+ngOnChanges(){
+
+}
 
 }
---
+++ src/app/services/pet.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Mascota } from '../model/mascota';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  findById(id: number): Observable<Mascota>{
+
+    return  this.http.get<Mascota>('http://localhost:8090/mascota/find/'+ id);
+
+  }
+
+  findAll(): Observable<Mascota[]> {
+    return this.http.get<Mascota[]>('http://localhost:8090/mascota/all');
+  }
+
+  findClientPet(id: number): Observable<Mascota[]>{
+    return this.http.get<Mascota[]>('http://localhost:8090/mascota/findClient/'+ id);
+  }
+
+
+  deleteById(id: number) {
+    return this.http.delete('http://localhost:8090/mascota/delete/' + id);
+  }
+
+}
