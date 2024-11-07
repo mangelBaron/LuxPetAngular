93d90b5f10a397d4eca7587c2b9c38e3fdc1d1da
mangelBaron <miguelbaronm@hotmail.com>
2024-11-06 22:09:14 -0500

    Cambio de css en algunos componentes

--- src/app/components/Landing/info-landing/info-landing.component.ts
+++ src/app/components/Landing/info-landing/info-landing.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-info-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './info-landing.component.html',
   styleUrl: './info-landing.component.css'
 })
